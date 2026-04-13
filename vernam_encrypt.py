8170ca477effee0bdef78ebd9141357acb22325e
Paola Zefkiq <paolazefkiq@gmail.com>
2026-04-13 23:00:04 +0200

    Nisja e funksionit te enkriptimit

--- vernam_encrypt.py
+++ vernam_encrypt.py
@@ -3,3 +3,9 @@ import string
 
 def generate_key(length):
     return ''.join(random.choice(string.ascii_uppercase) for _ in range(length))
+
+def encrypt(plaintext, key):
+    result = ""
+    key_index = 0
+
+    plaintext = plaintext.upper()
