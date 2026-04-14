36e437e819edbdbbf5a2dd8a708d1408a36409ce
Ramë Hadergjonaj <rame.hadergjonaj@student.uni-pr.edu>
2026-04-14 15:39:13 +0200

    Implement Vernam decryption logic

--- vernam_decrypt.py
+++ vernam_decrypt.py
@@ -6,7 +6,18 @@ def decrypt(cipher, key):
 
     for c in cipher:
         if c.isalpha():
-            pass
+            if key_index >= len(key):
+                raise ValueError("Key length is not enough.")
+
+            k = key[key_index].upper()
+
+            c_num = ord(c) - ord('A')
+            k_num = ord(k) - ord('A')
+
+            p_num = (c_num - k_num) % 26
+            result += chr(p_num + ord('A'))
+
+            key_index += 1
         else:
             result += c
 
