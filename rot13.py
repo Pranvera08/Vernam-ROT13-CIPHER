13b18395fc12c30087db46b23aef4a29f977ca03
Orlind Bajraktari <orlind.bajraktari@student.uni-pr.edu>
2026-04-14 15:05:25 +0200

    Update rot13.py
    kushti

--- rot13.py
+++ rot13.py
@@ -3,3 +3,13 @@ def transform(text):
     ROT13 transformation (same for encrypt/decrypt)
     """
     output = []
+for ch in text:
+        if ch.isalpha():
+            shift = 13
+            start = ord('A') if ch.isupper() else ord('a')
+            new_char = chr((ord(ch) - start + shift) % 26 + start)
+            output.append(new_char)
+        else:
+            output.append(ch)
+
+    return "".join(output)
