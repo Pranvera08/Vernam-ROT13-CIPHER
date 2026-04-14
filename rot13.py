2515f4dd7d1d8ce73dc7cadbf33a4bbd237d98f6
Orlind Bajraktari <orlind.bajraktari@student.uni-pr.edu>
2026-04-14 15:19:23 +0200

    Update rot13.py
    1

--- rot13.py
+++ rot13.py
@@ -3,19 +3,3 @@ def transform(text):
     ROT13 transformation (same for encrypt/decrypt)
     """
     output = []
-for ch in text:
-        if ch.isalpha():
-            shift = 13
-            start = ord('A') if ch.isupper() else ord('a')
-            new_char = chr((ord(ch) - start + shift) % 26 + start)
-            output.append(new_char)
-        else:
-            output.append(ch)
-
-    return "".join(output)
-def encrypt(text):
-    return transform(text)
-
-
-def decrypt(text):
-    return transform(text)
