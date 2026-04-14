f481756788d04835c96a9f2518f035be141bb45a
Ramë Hadergjonaj <rame.hadergjonaj@student.uni-pr.edu>
2026-04-14 15:32:03 +0200

    Add text iteration for Vernam decryption

--- vernam_decrypt.py
+++ vernam_decrypt.py
@@ -2,4 +2,12 @@ def decrypt(cipher, key):
     result = ""
     key_index = 0
 
+    cipher = cipher.upper()
+
+    for c in cipher:
+        if c.isalpha():
+            pass
+        else:
+            result += c
+
     return result
